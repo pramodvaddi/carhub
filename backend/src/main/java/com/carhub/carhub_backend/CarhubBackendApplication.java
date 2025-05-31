783896be039b9d5dc0f6eabbbca41e5428dda3cb
Pramod Vaddiraju <pramodvaddiraju@gmail.com>
2025-05-31 15:41:55 +0530

    Added frontend and created a backend

---
+++ backend/src/main/java/com/carhub/carhub_backend/CarhubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carhub.carhub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarhubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarhubBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/carhub/carhub_backend/CarhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carhub.carhub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarhubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
